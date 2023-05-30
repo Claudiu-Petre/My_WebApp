c5751696c8eff2c186a337f11f1bddcdce2c72ec
Claudiu Petre <claudiu.gpetre@gmail.com>
2023-05-30 15:23:33 +0100

    first one

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Webpage", page_icon="👾", layout="wide")
+
+# ----HEADER-----
+st.subheader("Hi, my name is Claudiu 🖐")
+st.title("A Software Engineer from Leeds, UK")
+st.write("I am passionate about programming and amazed about how a few lines of code can change your life...and others 😂 ")
+st.write("[Learn more >]()")
