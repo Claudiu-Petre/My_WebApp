b4192653cbe59c26e270b4ea9fb8f52f47e2169b
Claudiu Petre <claudiu.gpetre@gmail.com>
2023-05-30 16:26:59 +0100

    in pocess

--- app.py
+++ app.py
@@ -3,7 +3,26 @@ import streamlit as st
 st.set_page_config(page_title="My Webpage", page_icon="👾", layout="wide")
 
 # ----HEADER-----
-st.subheader("Hi, my name is Claudiu 🖐")
-st.title("A Software Engineer from Leeds, UK")
-st.write("I am passionate about programming and amazed about how a few lines of code can change your life...and others 😂 ")
-st.write("[Learn more >]()")
+with st.container():
+    st.subheader("Hi, my name is Claudiu 🖐")
+    st.title("Junior Software Engineer from Leeds, UK 🇬🇧")
+    st.subheader("JAVASCRIPT, REACT, EXPRESS, HTML, CSS, PYTHON")
+    st.write("I am passionate about programming and amazed about how a few lines of code can change your life...and others 😂 ")
+    st.write("[Learn more >](https://github.com/Claudiu-Petre)")
+    st.markdown("")
+
+# ----WHAT I DO----
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("What I do")
+        st.write("##")
+        st.write(""" I am transitioning from Site managing into software development through a variety of:
+        - courses,
+        - tutorials, 
+        - boot camps, 
+        - books, etc. 
+
+    Also, looking for opportunities that would allow me full time software development experience for a better and more versatile understanding of programming.
+        """)
