8ee537d354abf81f804d84212e9e1e2a39d5393b
Claudiu Petre <claudiu.gpetre@gmail.com>
2023-06-06 07:09:51 +0100

    css

--- app.py
+++ app.py
@@ -11,6 +11,14 @@ def load_lottieurl(url):
         return None
     return r.json()
 
+# USe local CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+local_css("style/app.css")
+
+    
+
 # --- Load assets---
 lottie_pic = load_lottieurl("https://assets9.lottiefiles.com/packages/lf20_7X0d6AzODk.json")
 img_1 = Image.open("Images/cruise.png")
@@ -34,11 +42,11 @@ with st.container():
         st.header("What I do")
         st.write("##")
         st.write(""" I am transitioning from Site managing into software development through a variety of: 
-        - courses,
-        - tutorials,
-        - boot camps, 
-        - books,
-        -  etc. 
+    - courses,
+    - tutorials,
+    - boot camps, 
+    - books,
+    -  etc. 
 
     Also, looking for opportunities that would allow me full time software development experience for a better and more versatile understanding of programming.
         """)
@@ -61,11 +69,34 @@ with st.container():
             """Learn how to navigate using Cruise ships GUI""")        
         st.markdown("[Find it here...](https://github.com/Claudiu-Petre/Cruise-ships)")
 
+st.write("---")
+
 with st.container():
     image_column, text_column = st.columns((1, 2))
     with image_column:
         st.image(img_2)
     with text_column:
-        st.subheader('')
+        st.subheader('"Leaflet" - a geolocation library in JS')
         st.write("""Learn how to track your favorite sports using Mapper""")
         st.markdown('[Find it here...](https://github.com/Claudiu-Petre/mapper)')
+
+# ---- Contact----
+with st.container():
+    st.write("---")
+    st.header("Get in touch with me!")
+    st.write("##")
+    #Documentation: https://formsubmit.co/
+    contact_form = """
+    <form action="https://formsubmit.co/claudiu.gpetre@gmail.com" method="POST">
+    <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your email" required>
+     <textarea name="message" placeholder="Your message here" required></textarea>
+     <button type="submit">Send</button>
+</form>
+    """
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.markdown(contact_form, unsafe_allow_html=True)
+    with right_column:
+        st.empty()
